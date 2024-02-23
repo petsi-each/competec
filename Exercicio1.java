4471ec2195c69405992ea30f1951e44977050f7b
Edu Paz <jooeduardo8@gmail.com>
2024-02-23 17:50:51 -0300

    adiciona exercícios

---
+++ Exercicio1.java
@@ -0,0 +1,17 @@
+/**
+ * Exercício 1 - Fácil - Slide 4
+ * Escreva um programa que guarde seu nome em uma variável chamada `nome`.
+ * Execute e veja o resultado no console.
+ */
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        // Escreva seu código aqui ⬇️
+
+
+
+        // Não altere o código abaixo
+
+        System.out.println("Olá, meu nome é " + nome);
+    }
+}
