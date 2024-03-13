946d41ba9d00dd53088ff5221c2e311b68e46b17
Matheus <matheusslcosta@usp.br>
2024-03-12 21:27:50 -0300

    Adicionando as pastas dos exercícios e os arquivos com as instruções.

---
+++ aula_5/Exercício2/Exercicio2.java
@@ -0,0 +1,20 @@
+/**
+ * Exercício 2 - Fácil - Slide 5 e 6
+ * Escreva um programa que pergunte o nome de usuário usando Scanner
+ * e então exiba:
+ *
+ * `Olá, [nome], seu nome tem [tamanho] caracteres.`
+ *
+ * Exemplo:
+ *
+ * > Qual é o seu nome?
+ * < João
+ * > Olá, João, seu nome tem 4 caracteres.
+ */
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        // Escreva seu código aqui ⬇️
+
+    }
+}
---
+++ aula_5/Exercício4/Exercicio4.java
@@ -0,0 +1,28 @@
+/**
+ * Exercício 4 - Difícil - Slide 9 e 10
+ *
+ * Escreva um programa que pergunte para o usuário o seu nome,
+ * e então exiba na tela o nome fornecido com a primeira letra maiúscula, e as restantes minúsculas,
+ * independente de como o usuário digitou o nome.
+ *
+ * Dica: pesquise sobre o método toLowerCase e toUpperCase da classe String e da classe Character.
+ * Dica: pesquise sobre o método substring da classe String.
+ *
+ * Exemplo:
+ *
+ * > Qual é o seu nome?
+ * < jOãO
+ * > Seu nome é João
+ *
+ * Extras:
+ * [ ] Permita que o usuário digite o nome completo, e exiba na tela o nome completo
+ *     com a primeira letra de cada palavra maiúscula, e as restantes minúsculas.
+ *     Para tal, pesquise sobre o método split da classe String.
+ */
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        // Escreva seu código aqui ⬇️
+
+    }
+}
