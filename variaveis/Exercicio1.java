7d50b4a9b7e8811ae0233663a9f0e4fbf2339ef2
Edu Paz <jooeduardo8@gmail.com>
2024-04-12 09:40:16 -0300

    adiciona exercicios aula 0

---
+++ variaveis/Exercicio1.java
@@ -0,0 +1,13 @@
+/*
+1. Crie uma variável que informa a primeira letra do seu nome;
+2. Crie uma variável que informa a sua idade;
+3. Crie uma variável informando se você tomou café da manhã;
+*/
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        // Escreva seu código aqui ⬇️
+
+
+    }
+}
---
+++ variaveis/Exercicio2.java
@@ -0,0 +1,13 @@
+/*
+1. Crie uma variável que armazena o valor de uma laranja;
+2. Crie uma variável que informa o valor de uma uva;
+3. Some o valor da laranja com a uva;
+*/
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        // Escreva seu código aqui ⬇️
+
+        
+    }
+}
