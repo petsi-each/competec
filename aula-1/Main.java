df88d19c4ea35a45c7e1165b983003a51fa2e87f
brennoromero <brennoromero06@gmail.com>
2025-05-10 11:58:08 -0300

    Update Main.java

--- aula-1/Main.java
+++ aula-1/Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	
+	 Scanner entrada = new Scanner(System.in);
+        
+	    System.out.print("Digite o seu nome:");
+	    String meuNome = entrada.nextLine();
+
+	    System.out.print("Digite o nome do seu:");
+	    String nomeDoAmigo = entrada.nextLine();
+	    
+	System.out.println(meuNome != nomeDoAmigo);
+	
+	
 	}
 }
